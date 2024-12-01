8bed38e4ecb14a561e68be40b1c1b928dda69a50
Alexander Sumiński <30773137+alex-suminski@users.noreply.github.com>
2024-12-01 01:40:35 +0100

    f

--- app/services/TaskInputFetcher.rb
+++
@@ -1,7 +0,0 @@
-class TaskInputFetcher
-  def call
-    response = HTTParty.get('https://adventofcode.com/2023/day/1/input', headers: {'Cookie' => Secrets.session_cookie})
-
-    response.body
-  end
-end
