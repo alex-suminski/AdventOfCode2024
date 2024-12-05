affa1e80ff3b3652c4683c33e80014653b16b0f8
Alexander Sumiński <30773137+alex-suminski@users.noreply.github.com>
2024-12-05 17:59:30 +0100

    solve day 5.2

--- app/services/day5.rb
+++ app/services/day5.rb
@@ -6,12 +6,38 @@ class Day5
   end
 
   def part1
-    input
+    sum_middle
+  end
+
+  def part2
     ordering_raw, programs_raw = input.split("")
     ordering = ordering_raw.map { |rule| rule.split('|').map(&:to_i) }.group_by(&:shift).transform_values(&:flatten)
     programs = programs_raw.map { |program| program.split(',').map(&:to_i) }
     programs.sum { |program|
       all_good = true
+      program.each_with_index { |page, i|
+        if shit?(page, i, program, ordering)
+          program = sort_shit(page, i, program, ordering)
+          all_good = false
+        end
+      }
+      if all_good
+        0
+      else
+        program[(program.size - 1) / 2]
+      end
+    }
+  end
+
+  def sum_middle
+    ordering_raw, programs_raw = input.split("")
+    ordering = ordering_raw.map { |rule| rule.split('|').map(&:to_i) }
+                           .group_by(&:shift)
+                           .transform_values(&:flatten)
+
+    programs_raw.sum { |program_raw|
+      all_good = true
+      program = program_raw.split(',').map(&:to_i)
       program.each_with_index { |page, i|
         all_good = false if shit?(page, i, program, ordering)
       }
@@ -23,15 +49,17 @@ class Day5
     }
   end
 
-  def part2
-    # input
-  end
-
   def shit?(page, i, program, ordering)
     if (followers = ordering[page])
       followers.intersection(program[0..i]).present?
     else
       false
     end
+  end
+
+  def sort_shit(page, i, program, ordering)
+    rule = ordering[page]
+    remove, leave = program[0..i - 1].partition { |el| rule.include?(el) }
+    leave + [page] + remove + program[(i + 1)..]
   end
 end
