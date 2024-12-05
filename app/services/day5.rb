d5803b0a9b4bf5d4e56640c216c33ff207756aa4
Alexander Sumiński <30773137+alex-suminski@users.noreply.github.com>
2024-12-05 17:39:55 +0100

    solve day 5.1

--- app/services/day5.rb
+++ app/services/day5.rb
@@ -7,9 +7,31 @@ class Day5
 
   def part1
     input
+    ordering_raw, programs_raw = input.split("")
+    ordering = ordering_raw.map { |rule| rule.split('|').map(&:to_i) }.group_by(&:shift).transform_values(&:flatten)
+    programs = programs_raw.map { |program| program.split(',').map(&:to_i) }
+    programs.sum { |program|
+      all_good = true
+      program.each_with_index { |page, i|
+        all_good = false if shit?(page, i, program, ordering)
+      }
+      if all_good
+        program[(program.size - 1) / 2]
+      else
+        0
+      end
+    }
   end
 
   def part2
-    input
+    # input
+  end
+
+  def shit?(page, i, program, ordering)
+    if (followers = ordering[page])
+      followers.intersection(program[0..i]).present?
+    else
+      false
+    end
   end
 end
--- spec/services/day5_spec.rb
+++ spec/services/day5_spec.rb
@@ -5,7 +5,34 @@ RSpec.describe Day5 do
 
   let(:task_input_fetcher_double) { instance_double(TaskInputFetcher) }
   let(:example) do
-    ''.split("\n")
+    '47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47'.split("\n")
   end
 
   before do
@@ -16,7 +43,7 @@ RSpec.describe Day5 do
     expect(day.part1).to eq 0
   end
 
-  it 'calculates part 2' do
+  xit 'calculates part 2' do
     expect(day.part2).to eq 0
   end
 end
