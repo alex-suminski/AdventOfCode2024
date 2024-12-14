c1b32fed0482c09ebdea9e9128cb47b8e145404a
Alexander Sumiński <30773137+alex-suminski@users.noreply.github.com>
2024-12-14 07:33:51 +0100

    math 13.2 wip

--- app/services/day13.rb
+++ app/services/day13.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+
 class Day13
   attr_reader :input
 
@@ -6,53 +8,82 @@ class Day13
   end
 
   def part1
-    cost = 0
-    input.each_slice(4) do |clawA, clawB, prize, _|
-      alt_costs = []
-      xA, yA = clawA.scan(/[X][+](\d{1,}), [Y][+](\d{1,})/).first.map(&:to_i)
-      xB, yB = clawB.scan(/[X][+](\d{1,}), [Y][+](\d{1,})/).first.map(&:to_i)
-      xP, yP = prize.scan(/[X][=](\d{1,}), [Y][=](\d{1,})/).first.map(&:to_i)
-      if xA >= (xB * 3) || yA >= (yB * 3)
-        puts clawA, clawB, prize, _
-        (0..100).map do |i|
-          move_xB = xB * i
-          move_yB = yB * i
-          if move_xB > xP || move_yB > yP
-            break
-          elsif ((xP - move_xB) % xA == 0) &&
-                ((yP - move_yB) % yA == 0) &&
-                (((xP - move_xB) / xA) == ((yP - move_yB) / yA))
-            # ((xP - move_xB) / xA) == ((yP - move_yB) / yA)
-            alt_costs << (i + (((xP - move_xB) / xA) * 3))
-          else
-            next
-          end
-        end
-      end
-      (0..100).map do |i|
-        move_xA = xA * i
-        move_yA = yA * i
-        if move_xA > xP || move_yA > yP
-          break
-        elsif ((xP - move_xA) % xB == 0) &&
-              ((yP - move_yA) % yB == 0) &&
-              (((xP - move_xA) / xB) == ((yP - move_yA) / yB))
-          alt_costs << (i * 3) + ((xP - move_xA) / xB)
-          # break
-        else
-          next
-        end
-      end
-      puts "==========================="
-      print alt_costs
-      puts "==========================="
-      cost += alt_costs.min || 0
-    end
-    cost
+    # cost = 0
+    # input.each_slice(4) do |clawA, clawB, prize, _|
+    #   xA, yA = clawA.scan(/[X][+](\d{1,}), [Y][+](\d{1,})/).first.map(&:to_i)
+    #   xB, yB = clawB.scan(/[X][+](\d{1,}), [Y][+](\d{1,})/).first.map(&:to_i)
+    #   xP, yP = prize.scan(/[X][=](\d{1,}), [Y][=](\d{1,})/).first.map(&:to_i)
+
+    #   (0..100).map do |i|
+    #     move_xA = xA * i
+    #     move_yA = yA * i
+    #     if move_xA > xP || move_yA > yP
+    #       break
+    #     elsif ((xP - move_xA) % xB == 0) &&
+    #           ((yP - move_yA) % yB == 0) &&
+    #           (((xP - move_xA) / xB) == ((yP - move_yA) / yB))
+    #       cost += (i * 3) + ((xP - move_xA) / xB)
+    #       break
+    #     else
+    #       next
+    #     end
+    #   end
+    # end
+
+    # cost
+    math
   end
 
   def part2
-    input
-    0
+    math(shift: 10000000000000)
+  end
+
+  # def punkt_przeciecia_z_x(y, x, a)
+  #   -((y - x * a) / a)
+  # end
+
+  def math(shift: 0)
+    time = Time.current
+    cost = 0
+
+    input.each_slice(4) do |clawA, clawB, prize, _|
+      xA, yA = clawA.scan(/[X][+](\d{1,}), [Y][+](\d{1,})/).first.map(&:to_f)
+      xB, yB = clawB.scan(/[X][+](\d{1,}), [Y][+](\d{1,})/).first.map(&:to_f)
+      xP, yP = prize.scan(/[X][=](\d{1,}), [Y][=](\d{1,})/).first.map { |z| z.to_i + shift }
+
+      aB = yB / xB
+      bB = 0.0
+
+      aA = yA / xA
+
+      # x0 = punkt_przeciecia_z_x(yP, xP, aA)
+      bA = find_b(xP, yP, aA)
+
+      x, y = punkt_przeciecia(aA, aB, bA, bB)
+      # debugger
+      prec = 4
+      if (x.round == x.round(prec)) && (y.round == y.round(prec))
+        b_count = x.round / xB
+        a_count = (xP - x.round) / xA
+        cost += (b_count + a_count * 3)
+      end
+    end
+    puts (Time.current - time)
+    cost
+  end
+
+  def find_b(x, y, a)
+    y - a * x
+  end
+
+  def punkt_przeciecia(a1, a2, b1, b2)
+    if a1 == a2
+      raise "zero"
+    end
+
+    x = (b2 - b1) / (a1 - a2)
+    y = a1 * ((b2 - b1) / (a1 - a2)) * x + b1
+
+    [x, y]
   end
 end
