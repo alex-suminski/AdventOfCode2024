53c2d92435f56ac7a3b47de95488b32f81c7737e
Alexander Sumiński <30773137+alex-suminski@users.noreply.github.com>
2024-12-14 09:09:54 +0100

    solve 14

--- app/services/day14.rb
+++ app/services/day14.rb
@@ -2,15 +2,70 @@ class Day14
   attr_reader :input
 
   def initialize(task_input_fetcher: TaskInputFetcher.new)
-    @input = task_input_fetcher.call(10)
+    @input = task_input_fetcher.call(14)
   end
 
-  def part1
-    input
+  def part1(height: 103, width: 101, seconds: 100)
+    midx = (width - 1) / 2
+    midy = (height - 1) / 2
+    q1 = q2 = q3 = q4 = 0
+    input.map do |robot|
+      p, v = robot.split()
+      px, py = p.split('=').last.split(',').map(&:to_i)
+      vx, vy = v.split('=').last.split(',').map(&:to_i)
+
+      fx = (px + (seconds * vx)) % width
+      fy = (py + (seconds * vy)) % height
+      if fx < midx && fy < midy
+        q1 += 1
+      elsif fx > midx && fy < midy
+        q2 += 1
+      elsif fx < midx && fy > midy
+        q3 += 1
+      elsif fx > midx && fy > midy
+        q4 += 1
+      end
+    end
+    [q1, q2, q3, q4].reduce(:*)
   end
 
-  def part2
-    input
-    0
+  def part2(height: 103, width: 101, seconds: 10000)
+    first_line = (0..width).map { |x| [x, 0] }
+    (7000..seconds).each do |seconds|
+      maybe_tree = input.map do |robot|
+        p, v = robot.split()
+        px, py = p.split('=').last.split(',').map(&:to_i)
+        vx, vy = v.split('=').last.split(',').map(&:to_i)
+
+        fx = (px + (seconds * vx)) % width
+        fy = (py + (seconds * vy)) % height
+        [fx, fy]
+      end
+      puts seconds
+      if is_tree?(maybe_tree)
+        puts "------------------------------------------------------------------------------------------------------------------------"
+        light_tree(maybe_tree, height, width)
+        puts "------------------------------------------------------------------------------------------------------------------------"
+      end
+    end
+  end
+
+  def light_tree(tree, height, width)
+    (0..height).each do |y|
+      (0..width).each do |x|
+        if tree.include?([x, y])
+          print '*'
+        else
+          print ' '
+        end
+      end
+      print "\n"
+    end
+  end
+
+  def is_tree?(tree)
+    tree.group_by(&:second).values.any? do |bobbles|
+      bobbles.count > 30
+    end
   end
 end
--- spec/services/day14_spec.rb
+++ spec/services/day14_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe Day14 do
 
   let(:task_input_fetcher_double) { instance_double(TaskInputFetcher) }
 
-  context 'when 1 example' do
+  context 'when basic' do
     let(:example) do
-      ''.split("\n")
+      'p=2,4 v=2,-3
+'.split("\n")
     end
 
     before do
@@ -15,11 +16,36 @@ RSpec.describe Day14 do
     end
 
     it 'calculates part 1' do
-      expect(day.part1).to eq 1
+      expect(day.part1(height: 7, width: 11, seconds: 5)).to eq 0
+    end
+  end
+
+  context 'when 1 example' do
+    let(:example) do
+      'p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3'.split("\n")
+    end
+
+    before do
+      allow(task_input_fetcher_double).to receive(:call).and_return(example)
+    end
+
+    it 'calculates part 1' do
+      expect(day.part1(height: 7, width: 11)).to eq 12
     end
 
     it 'calculates part 2' do
-      expect(day.part2).to eq 0
+      expect(day.part2(height: 7, width: 11, seconds: 5)).to eq 0
     end
   end
 end
